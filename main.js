9808ad59a8d2cd55fc6d2425573cca5a1dde09f5
Prasant <67827472+prasantkumar123@users.noreply.github.com>
2021-05-28 15:57:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,114 +1,424 @@
-//var reg = /### Problem/ig;
-//console.log(problem.search(reg))
-//console.log(problem.split('### Problem')[2])
-//console.log(problem.search(reg));
-
-
-//import * as prblm from './problem.js'
-//console.log(prblm.problem.length)
-
 window.addEventListener('load', function(){
+
  // console.log('hi')
+
  var count = 0;
+
  var searchIcon = document.getElementById('searchIcon');
+
  var searchBox = document.getElementById('searchBox');
+
  searchIcon.addEventListener('click', function(){
+
    count++;
+
    if(count % 2 != 0){
+
      searchBox.style.height = '8rem';
+
      searchBox.style.padding = '15px';
+
    }else{
+
       searchBox.style.height = '0px';
+
        searchBox.style.padding = '0px';
 
    }
+
   // console.log(count);
+
  });
+
  
+
+ 
+
+ 
+
+ // collecting problems and solutions in an array
+
+ 
+
+ //array for problems
+
  var problemArr = problem.split('### Problem')
- var regEnd = /### Problem/ig;
- var endPoints = regEnd.exec(problem);
- //console.log(endPoints);
+
  
+
+ //array for solutions
+
+ var solutionArr = [];
+
+ for(let i = 0; i < problemArr.length; i++){
+
+   if (i < 10) {
+
+     solutionArr.push(`https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_00${i}.py`);
+
+   } else if (i >= 10 && i < 100) {
+
+     solutionArr.push(`https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_0${i}.py`);
+
+   } else {
+
+     solutionArr.push(`https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_${i}.py`);
+
+   }
+
+ }
+
+ 
+
+ 
+
+ //getting html elements
+
+ 
+
  var mainProblem = document.getElementById('mainProblem');
+
  
+
  
+
  var numSearch = document.getElementById('numSearch');
+
   var strSearch = document.getElementById('strSearch');
+
   var num = document.getElementById('num');
+
   var str = document.getElementById('str');
+
   
+
   var solution = document.getElementById('solution');
+
   
+
   var solLink = document.getElementById('solLink');
+
  
+
  
+
  
+
  
+
  
+
  // Function for search by number
+
   numSearch.addEventListener('click', function () {
-    if(problemArr[num.value] != undefined){
-      mainProblem.innerText = problemArr[parseInt(num.value)];
-    solution.style.display = 'flex';
-    if(num.value < 10){
-      solLink.href = `https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_00${num.value}.py`;
-    }else if(num.value >= 10 && num.value < 100){
-      solLink.href = `https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_0${num.value}.py`;
-    }else{
-      solLink.href = `https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_${num.value}.py`;
-    }
+
+    var numVal = parseInt(num.value);
+
+    if(problemArr[numVal] != undefined){
+
+      mainProblem.innerText = problemArr[numVal];
+
     
-   // console.log(solLink.href)
+
+    solution.style.display = 'flex';
+
+    
+
+    if(numVal < 10){
+
+      solLink.href = `https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_00${numVal}.py`;
+
+    }else if(numVal >= 10 && numVal < 100){
+
+      solLink.href = `https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_0${numVal}.py`;
+
     }else{
+
+      solLink.href = `https://raw.githubusercontent.com/vineetjohn/daily-coding-problem/master/solutions/problem_${numVal}.py`;
+
+    }
+
+   // console.log(solutionArr[numVal]);
+
+    
+
+   // console.log(solLink.href)
+
+    }else{
+
       mainProblem.innerHTML = `
-      <p class='err' align='center'>Please enter an integer from 0 to ${problemArr.length - 1}</p>
+
+      <p class='err' align='center'>Please enter an integer from 1 to ${problemArr.length - 1}</p>
+
       
+
         <div class="landPage">
-      <h1 class="mainHead" align="center">Smash Problems</h1>
+
+      <div class="heads">
+
+              <h1 class="mainHead" align="center">Smash Problems</h1>
+
+            <p class="subhead" align="center">smash at a glance</p>
+
+           </div>
+
       <p class="info" align="center">
+
         Click the search icon at the top right corner to search a problem.
+
       </p>
+
     </div>
 
       `;
+
         solution.style.display = 'none';
 
-      // console.log(typeof(num.value));
-     // console.log(problemArr[01])
-     // console.log(problemArr[num.value])
+     
+
     }
+
     num.value = '';
+
     str.value = '';
+
     searchBox.style.height = '0px';
+
        searchBox.style.padding = '0px';
+
        count++;
+
     //console.log(problemArr.length)
+
   })
+
   
+
   
+
   
+
  
+
   
+
   
+
   
+
   // Function for search by string
 
-/*
   strSearch.addEventListener('click', function(){
-var regStr = new RegExp(str, 'ig');
 
-var arr = [];
-let t;
-  while(t = regStr.exec(problem)){
-   arr.push(t.index)
+    
+
+    mainProblem.innerHTML = '';
+
+   solution.style.display = 'none';
+
+   var userRegex = str.value;
+
+if(str.value.includes('.') || str.value.includes('*')){
+
+  //console.log('includes !');
+
+ userRegex = str.value.replace(/\./g, "\\.").replace(/\*/g, "\\*");
+
+ //console.log(userRegex);
+
+ 
+
+}
+
+// var regStr = new RegExp(userRegex, 'ig');
+
+//console.log(regStr);
+
+var textStr = '';
+
+var userText = str.value.replace(/^\s+/, '').replace(/\s+$/, '');
+
+if(userText == ''){
+
   
- }
-// console.log(arr);
- //console.log(problem.match(regStr));
-mainProblem.innerHTML = arr;
+
+      mainProblem.innerHTML = `
+
+       <p class='err' align='center'> Hey ! what are you looking for ? You are searching an empty string hehe </p>
+
+      
+
+        <div class="landPage">
+
+      <div class="heads">
+
+              <h1 class="mainHead" align="center">Smash Problems</h1>
+
+            <p class="subhead" align="center">smash at a glance</p>
+
+           </div>
+
+      <p class="info" align="center">
+
+        Click the search icon at the top right corner to search a problem.
+
+      </p>
+
+    </div>
+
+      `;
+
+      //console.log(mainProblem.innerHTML)
+
+num.value = '';
+
+    str.value = '';
+
+    searchBox.style.height = '0px';
+
+    //console.log(searchBox.style.height);
+
+       searchBox.style.padding = '0px';
+
+       count++;
+
+  return;
+
+}else if(userText == '.' || userText.match(/^[a-zA-Z0-9]$/) || userText.length < 2 || userText.match(/\\/) || userText.includes('\\')){
+
+ // console.log('only dot used');
+
+ mainProblem.innerHTML = ` <p class='err' align='center'> Please use more than one character and don't use backslash (\\) </p>
+
+      
+
+        <div class="landPage">
+
+      <div class="heads">
+
+              <h1 class="mainHead" align="center">Smash Problems</h1>
+
+            <p class="subhead" align="center">smash at a glance</p>
+
+           </div>
+
+      <p class="info" align="center">
+
+        Click the search icon at the top right corner to search a problem.
+
+      </p>
+
+    </div>`;
+
+    
+
+    num.value = '';
+
+    str.value = '';
+
+    searchBox.style.height = '0px';
+
+   // console.log(searchBox.style.height);
+
+       searchBox.style.padding = '0px';
+
+       count++;
+
+    
+
+  return;
+
+}
+
+//regEx and for loop to add problem on match
+
+var regStr = new RegExp(userRegex, 'ig');
+
+for(let i = 0; i < problemArr.length; i++){
+
+  if(problemArr[i].match(regStr) != null){
+
+    
+
+    textStr = problemArr[i].replace(regStr, strReplacer).replace(/ /mg,"&nbsp;").replace(/(\r\n|\n|\r)/gm, "<br>");
+
+    
+
+    mainProblem.innerHTML += textStr +'<br>' +
+
+      ` <a href='${solutionArr[i]}' class="strSolLink" >  <div class="strSolution">Solution (python) <br />
+
+     
+
+    </div>
+
+   
+
+    </a>` +
+
+    '<br> <hr> <br>';
+
+   
+
+    
+
+}
+
+    
+
+  }
+
+  if(textStr == ''){
+
+    mainProblem.innerHTML = ` <p class='err' align='center'> Oops ! No matches found !</p>
+
+      
+
+        <div class="landPage">
+
+      <div class="heads">
+
+              <h1 class="mainHead" align="center">Smash Problems</h1>
+
+            <p class="subhead" align="center">smash at a glance</p>
+
+           </div>
+
+      <p class="info" align="center">
+
+        Click the search icon at the top right corner to search a problem.
+
+      </p>
+
+    </div>`;
+
+  }
+
+num.value = '';
+
+    str.value = '';
+
+    searchBox.style.height = '0px';
+
+    //console.log(searchBox.style.height);
+
+       searchBox.style.padding = '0px';
+
+       count++;
+
   });
+
   
-*/
+
+  
+
+  
+
+  function strReplacer(inp) {
+
+  return "<mark>" + inp + "</mark>"
+
+}
+
 });
 
